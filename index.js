16479ba58f3fa521df79e650d7838604dd2bdfd2
samar <samarjeetb0@gmail.com>
2024-02-19 14:04:15 +0530

    add jenkins

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const express = require("express");
+const EventEmitter = require('events')
 const app = express();
 const PORT = process.env.PORT || 3000;
+const eventEmitter = new EventEmitter();
+
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+
+app.get("/event", (req, res) => {
+  eventEmitter.emit('event', res)
+});
+
+
+eventEmitter.on('event', (res)=> {
+
+  res.send(`Coming from event`)
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/hello", async (req, res) => {
+  try {
+    const result = await promiseHandle();
+    res.send(`Hello, World! from async & promise ${result}`);
+  } catch (error) {
+    console.log(error);
+    res.send(`Error `)
+
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Node app listening at port: ${PORT}`);
 });
+
+function promiseHandle() {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      // If the operation is successful, call resolve with the result
+      if (randomNumber > 10) {
+        reject(new Error("Random number is too low"));
+      } else {
+        // If there's an error, call reject with an error message
+        resolve(randomNumber);
+      }
+    }, 3000);
+  });
+
+  return promise;
+}
